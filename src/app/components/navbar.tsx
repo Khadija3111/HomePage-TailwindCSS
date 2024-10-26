3b5fbf02a75d43a761dcf9794876f23bb67dd80c
khadija <khadijjaa311@gmail.com>
2024-10-26 22:34:53 +0500

    Initial commit from Create Next App

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,34 @@
+export default function Navbar(){
+    return(
+<div >
+<nav className="bg-slate-900 h-24
+gap: 324px;
+opacity: 0px;
+
+ ">
+<h3 className="font-serif font-bold text-base  leading-8 tracking-normal "></h3>
+<ul> 
+    <li>Home</li>
+    <li>Product</li>
+    <li>Pricing</li>
+    <li>Contact</li>
+    <li>Login</li>
+
+
+
+
+</ul>
+
+</nav>
+
+
+
+
+
+
+
+
+</div>
+
+    )
+}
